549bf17d53152a4cb0b997b8c1f3955d30a2281b
alexander-melentyev <asmelentyev@yandex.ru>
2020-11-17 21:33:56 +0300

    refactor(03-unit-testing): go-err113 lint fix

--- 03-unit-testing/mock/store_test.go
+++ 03-unit-testing/mock/store_test.go
@@ -48,6 +48,8 @@ func TestDuplicate(t *testing.T) {
 	require.NotEqual(t, newID, user1.ID)
 }
 
+var errTest = errors.New("test error")
+
 func TestDuplicateErr(t *testing.T) {
 	mockCtl := gomock.NewController(t)
 	defer mockCtl.Finish()
@@ -61,10 +63,9 @@ func TestDuplicateErr(t *testing.T) {
 		Phone: "8-911-234-4567",
 	}
 
-	testErr := errors.New("test error")
 	mockDB.EXPECT().FindUser(user1.ID).Return(user1, nil)
-	mockDB.EXPECT().AddUser(gomock.Any()).Return(testErr)
+	mockDB.EXPECT().AddUser(gomock.Any()).Return(errTest)
 	_, err := store.Duplicate(user1.ID)
 
-	require.EqualError(t, err, testErr.Error())
+	require.EqualError(t, err, errTest.Error())
 }
