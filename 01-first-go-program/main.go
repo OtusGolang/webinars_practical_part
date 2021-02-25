24adbeb2b50591d0ead8c1d76557bc21adaa4614
Anton Telyshev <anton.telishev@yandex.ru>
2021-02-25 20:50:44 +0300

    Update main.go

--- 01-first-go-program/main.go
+++ 01-first-go-program/main.go
@@ -3,8 +3,8 @@ package main // Текущий пакет
 // Импортируем пакеты
 import (
 	"os" // Пакет для работы с ОС из стандартной библиотеки Go
-	// Пакет "searcher" нашего модуля "github.com/OtusGolang/webinars_practical_part/1.1_first_go_program"
-	"github.com/OtusGolang/webinars_practical_part/1.1_first_go_program/searcher"
+	// Пакет "searcher" нашего модуля "github.com/OtusGolang/webinars_practical_part/01-first-go-program"
+	"github.com/OtusGolang/webinars_practical_part/01-first-go-program/searcher"
 )
 
 // Константа уровня пакета
